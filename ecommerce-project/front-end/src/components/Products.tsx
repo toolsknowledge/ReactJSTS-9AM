a80ccd441073d199ad58e1732105d00b27c90b59
samba.pendela@gmail.com <samba.pendela@gmail.com>
2021-02-27 09:40:05 +0530

    ecommerce

--- ecommerce-project/front-end/src/components/Products.tsx
+++ ecommerce-project/front-end/src/components/Products.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import Product from "../modal/Product";
+import Rating from "./Rating";
 //it is receiving array
 //this component displays in the form of a "Card"
+
+//row
+//center
+//card
+//medium
+//card-body
+//price
 interface IProps{
     arr : Product[]
 };
@@ -13,7 +21,22 @@ class Products extends React.Component<IProps,IState>{
     render(){
         return(
             <React.Fragment>
-                { JSON.stringify(this.props.arr) }
+                <div className="row center">
+                    {this.props.arr.map((prod)=>(
+                        <div className="card" key={prod._id}>
+                            <a href="#">
+                                <img src={prod.image} className="medium" alt={prod.name}></img>
+                            </a>
+                            <div className="card-body">
+                                <h2>{prod.name}</h2>
+                                <Rating rating={prod.rating} numReviews={prod.numReviews}></Rating>
+                                <div className="price">
+                                    Price : ${prod.price}
+                                </div>
+                            </div>
+                        </div>
+                    ))}         
+                </div>
             </React.Fragment>
 
         )
