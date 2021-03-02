4e91ef6b47fa9a769670d29e9d4e0456f351b076
samba.pendela@gmail.com <samba.pendela@gmail.com>
2021-03-02 09:41:11 +0530

    detail screen started

--- ecommerce-project/front-end/src/screens/ProductScreen.tsx
+++ ecommerce-project/front-end/src/screens/ProductScreen.tsx
@@ -1,20 +1,52 @@
 import React from "react";
-interface IProps{}
+import { match as Match} from "react-router-dom";
+import getDetailProduct from "../actions/DetailsActions";
+import { connect } from "react-redux";
+import MessageBox from "../components/MessageBox";
+import LoadingBox from "../components/LoadingBox";
+interface IProps{
+    match : Match<routeParams>;
+    get_product:any;
+    productDetails:any;
+}
 interface IState{}
-
-
-
+interface routeParams{
+    id:any;
+};
 class ProductScreen extends React.Component<IProps,IState>{
     constructor(props:IProps){
         super(props);
     }
+    componentDidMount(){
+         this.props.get_product( this.props.match.params.id );
+    };
     render(){
+        const { loading,error,product } = this.props.productDetails;
         return(
             <React.Fragment>
-                Product Details Soon....!
+
+                { !loading ? (<LoadingBox />) : error === "Network Error"? 
+                  (<MessageBox variant="warning">{error}</MessageBox>) : JSON.stringify( this.props.productDetails ) }
+
+
             </React.Fragment>
         )
     }
 };
 
-export default ProductScreen;
+//subscription
+const receive = (state:any)=>{
+    return{
+        productDetails : state.details
+    }
+};
+
+//dispatch
+const send = (dispatch:any)=>{
+    return{
+        get_product : (id:any)=>{ dispatch( getDetailProduct(id) ) }
+    }
+};
+
+
+export default connect(receive,send)(ProductScreen);
